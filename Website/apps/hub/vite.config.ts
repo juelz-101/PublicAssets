be82fe6113192133cd0ceaaa4c930238fae25e1e
Juelz-101 <you@example.com>
2026-06-08 06:22:32 +0100

    r

--- Website/apps/hub/vite.config.ts
+++ Website/apps/hub/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
-	  base: '/PublicAssets/hub/',
+	  base: '/PublicAssets/Website/apps/hub/',
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
